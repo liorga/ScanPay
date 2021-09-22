fdbfab4c12744fc6ffabcf101929445334052d61
liorga <liorgal31892@gmail.com>
2021-09-22 13:14:58 +0300

    changes

--- db_models/user.js
+++ db_models/user.js
@@ -4,23 +4,36 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+
     required: true,
+
     minlength: 2,
+
     maxlength: 50,
   },
+
   email: {
     type: String,
+
     required: true,
+
     minlength: 5,
+
     maxlength: 255,
+
     unique: true,
   },
+
   password: {
     type: String,
+
     required: true,
+
     minlength: 5,
+
     maxlength: 1024,
   },
+
   isAdmin: Boolean,
 });
 
@@ -29,7 +42,9 @@ const User = mongoose.model('User', userSchema);
 function validateUser(user) {
   const schema = {
     name: Joi.string().min(2).max(50).required(),
+
     email: Joi.string().min(5).max(255).required().email(),
+
     password: Joi.string().min(5).max(255).required(),
   };
 
@@ -37,4 +52,5 @@ function validateUser(user) {
 }
 
 exports.User = User;
+
 exports.validate = validateUser;
