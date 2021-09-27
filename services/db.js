936b7b64269ab0b43e8dbc4162e5f86f001981a5
liorga <liorgal31892@gmail.com>
2021-09-27 16:19:49 +0300

    heroku update

--- services/db.js
+++ services/db.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
-const config = require('config');
 
 module.exports = function configDb() {
-  const db = config.get('db');
-  mongoose.connect(db).then(() => console.log(`Connected to ${db}...`));
+  // const db = config.get('db');
+  // mongoose.connect(db).then(() => console.log(`Connected to ${db}...`));
+  mongoose
+
+    .connect(process.env.MONGO_URI || 'mongodb://localhost', { useNewUrlParser: true, useUnifiedTopology: true })
+
+    .then(console.log('MongoDB connected'))
+
+    .catch((err) => console.log(err));
 };
